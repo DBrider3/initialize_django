61dbd424b0ffe6410d2e53fd5b89472c2759c634
DAEBEOM CHO <tmdgns743@gmail.com>
2024-02-25 18:25:08 +0900

    static root 추가

--- config/settings.py
+++ config/settings.py
@@ -141,6 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+
 
 MEDIA_URL = "media/"  # 접근할때 쓰이는 url
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")  # 저장할때 쓰이는 경로
